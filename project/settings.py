da5b0c0a0c2b5da9d7ff53dd73b645ce0dab2889
Gabriel Rocha Pimentel <gabrielrochapimentel.dev@gmail.com>
2024-06-24 15:38:50 -0300

    update models.py

--- project/settings.py
+++ project/settings.py
@@ -94,9 +94,15 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+
+# Configurações para arquivos de mídia
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
