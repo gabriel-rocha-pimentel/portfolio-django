0da5f1d5ab13cfd7ea9dae0e5b35e16444742c18
Gabriel Rocha Pimentel <gabrielrochapimentel.dev@gmail.com>
2024-06-24 16:28:36 -0300

    update styles files

--- project/settings.py
+++ project/settings.py
@@ -96,12 +96,12 @@ USE_TZ = True
 
 
 # Configurações para arquivos de mídia
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
 
 
